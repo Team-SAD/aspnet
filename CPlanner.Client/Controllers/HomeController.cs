c62e2c66c93b96d23f53f24af71576a57b1e8796
daniel henderson <drayhender@gmail.com>
2021-05-24 07:48:45 -0400

    local host update

--- CPlanner.Client/Controllers/HomeController.cs
+++ CPlanner.Client/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using CPlanner.Client.Models;
 
 namespace CPlanner.Client.Controllers
 {
+    [Route("[controller]")]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -30,17 +31,6 @@ namespace CPlanner.Client.Controllers
             // }
             return View("index");
 
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
